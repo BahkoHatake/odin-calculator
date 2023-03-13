95a5a1f0059d85b5bf69039e4eddf800ee9b03fa
Branko Cvetanović <cbranko01@gmail.com>
2023-03-13 22:49:29 +0100

    Make calculator work

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(x,y){
-    return x+y
+    return Number(x)+Number(y)
 }
 function subtract(x,y){
     return x-y
@@ -26,3 +26,36 @@ function operate(operator,x,y){
     }
     return res
 }
+let firstVar=""
+function fillFirstDisplay (){
+    firstVar+=this.value
+    display1.textContent=firstVar
+}
+let secondVar=""
+function makeSecondVar(){
+    secondVar+=this.value
+    display1.textContent=secondVar
+}
+const display1=document.querySelector(".display1")
+const numButtons=document.querySelectorAll(".num");
+numButtons.forEach(number=>number.addEventListener("click", fillFirstDisplay))
+ 
+let operatorVar=""
+let secondDisplay=""
+const display2=document.querySelector(".display2");
+const operatorButtons=document.querySelectorAll(".operator");
+operatorButtons.forEach(operator=>operator.addEventListener("click",()=>{
+    operatorVar=operator.value;
+    secondDisplay=firstVar+operatorVar;
+    display2.textContent=secondDisplay;
+    numButtons.forEach(number=>number.removeEventListener("click", fillFirstDisplay))
+    numButtons.forEach(number=>number.addEventListener("click",makeSecondVar))
+    })
+)
+
+let equals = document.querySelector(".equals");
+equals.addEventListener("click",()=>{
+    display2.textContent=secondDisplay+secondVar+"=";
+    let resoult=operate(operatorVar,firstVar,secondVar)
+    display1.textContent=resoult
+})
