d8c960b9758ebc258a9a28e3f03b73422b07d9e8
Branko Cvetanović <cbranko01@gmail.com>
2023-03-15 20:00:31 +0100

    fix the logic behind calculator, add clear button

--- script.js
+++ script.js
@@ -12,50 +12,81 @@ function divide(x,y){
 }
 function operate(operator,x,y){
     let res=null
-    if (operator==="+"){
+    if (y==="")
+        {return x}
+    else if (operator==="+"){
         res=add(x,y)
     }
     else if (operator==="-"){
         res=subtract(x,y)
     }
-    else if(operator==="*"){
+    else if(operator==="×"){
         res=multiply(x,y)
     }
-    else if(operator==="/"){
-        res= devide(x,y)
+    else if(operator==="÷"){
+        if(y==0){
+            alert("LOL, it is impossible to divide by ZERO")
+        }
+        res= divide(x,y)
     }
-    return res
+    return res.toFixed(8).replace(/\.?0+$/, "")
 }
-let firstVar=""
-function fillFirstDisplay (){
-    firstVar+=this.value
-    display1.textContent=firstVar
+
+let firstNum=""
+function fillFirstDisplayFirsTime (){
+    firstNum+=this.value;
+    display1.textContent=firstNum;
+    if (this.value==="."){
+        decimal.removeEventListener("click",fillFirstDisplayFirsTime)}
 }
-let secondVar=""
-function makeSecondVar(){
-    secondVar+=this.value
-    display1.textContent=secondVar
-}
+const decimal=document.querySelector(".decimal")
 const display1=document.querySelector(".display1")
 const numButtons=document.querySelectorAll(".num");
-numButtons.forEach(number=>number.addEventListener("click", fillFirstDisplay))
- 
-let operatorVar=""
-let secondDisplay=""
+numButtons.forEach(number=>number.addEventListener("click", fillFirstDisplayFirsTime))
+let operatorVar="";
+let currentNum="";
+let secondNum="";
+let currentOperation="";
+function addEventListenerOperator(){
+    operatorVar=this.value;
+    if (operatorVar===currentOperation||currentOperation==="") {   
+        currentNum=operate(operatorVar,firstNum,secondNum);
+        display2.textContent=currentNum+operatorVar;}
+    else{
+        currentNum=operate(currentOperation,firstNum,secondNum);
+        display2.textContent=currentNum+operatorVar;
+    }
+    display1.textContent="_";
+    firstNum=currentNum;
+    secondNum="";
+    currentOperation=operatorVar
+    numButtons.forEach(number=>number.removeEventListener("click", fillFirstDisplayFirsTime))
+    numButtons.forEach(number=>number.addEventListener("click",makeSecondVar))
+}
+function makeSecondVar(){
+    secondNum+=this.value;
+    display1.textContent=secondNum;
+    if (this.value==="."){
+        decimal.removeEventListener("click",makeSecondVar)}
+}
 const display2=document.querySelector(".display2");
 const operatorButtons=document.querySelectorAll(".operator");
-operatorButtons.forEach(operator=>operator.addEventListener("click",()=>{
-    operatorVar=operator.value;
-    secondDisplay=firstVar+operatorVar;
-    display2.textContent=secondDisplay;
-    numButtons.forEach(number=>number.removeEventListener("click", fillFirstDisplay))
-    numButtons.forEach(number=>number.addEventListener("click",makeSecondVar))
-    })
-)
+operatorButtons.forEach(operator=>operator.addEventListener("click",addEventListenerOperator));
 
 let equals = document.querySelector(".equals");
 equals.addEventListener("click",()=>{
-    display2.textContent=secondDisplay+secondVar+"=";
-    let resoult=operate(operatorVar,firstVar,secondVar)
-    display1.textContent=resoult
+    if(secondNum===""){
+        display2.textContent=firstNum+"=";
+    }
+    else{display2.textContent=firstNum+currentOperation+secondNum+"=";}
+    let result=operate(currentOperation,firstNum,secondNum)
+    display1.textContent=result
+    numButtons.forEach(number=>number.removeEventListener("click",makeSecondVar));
+    numButtons.forEach(number=>number.removeEventListener("click", fillFirstDisplayFirsTime))
 })
+
+const clear=document.querySelector(".clear");
+clear.addEventListener("click",()=>{location.reload()})
+
+
+
