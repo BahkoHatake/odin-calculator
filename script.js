2d5b822578f12b92a64304da6ef9eb2a7a622627
Branko Cvetanović <cbranko01@gmail.com>
2023-03-15 22:44:07 +0100

    Add Style to calculator

--- script.js
+++ script.js
@@ -1,3 +1,79 @@
+let firstNum=""
+const decimal=document.querySelector(".decimal")
+const display1=document.querySelector(".display1")
+function fillFirstDisplayFirsTime (){
+    firstNum+=this.value
+    display1.textContent=firstNum;
+    if (this.value==="."){
+        decimal.removeEventListener("click",fillFirstDisplayFirsTime)}
+}
+const numButtons=document.querySelectorAll(".num");
+numButtons.forEach(number=>number.addEventListener("click", fillFirstDisplayFirsTime))
+
+let operatorVar="";
+let currentNum="";
+let secondNum="";
+let currentOperation="";
+function addEventListenerOperator(){
+    operatorVar=this.value;
+    if (operatorVar===currentOperation||currentOperation==="") {   
+        currentNum=operate(operatorVar,firstNum,secondNum);
+        display2.textContent=currentNum+operatorVar;}
+    else{
+        currentNum=operate(currentOperation,firstNum,secondNum);
+        display2.textContent=currentNum+operatorVar;
+    }
+    display1.textContent="";
+    firstNum=currentNum;
+    secondNum="";
+    currentOperation=operatorVar
+    numButtons.forEach(number=>number.removeEventListener("click", fillFirstDisplayFirsTime))
+    numButtons.forEach(number=>number.addEventListener("click",makeSecondVar))
+}
+function makeSecondVar(){
+    secondNum+=this.value;
+    display1.textContent=secondNum;
+    if (this.value==="."){
+        decimal.removeEventListener("click",makeSecondVar)}
+}
+const display2=document.querySelector(".display2");
+const operatorButtons=document.querySelectorAll(".operator");
+operatorButtons.forEach(operator=>operator.addEventListener("click",addEventListenerOperator));
+
+function evaluate(){
+    if(firstNum===""){
+        return
+    }
+    if(secondNum===""){
+        display2.textContent=firstNum+"=";
+    }
+    else{display2.textContent=firstNum+currentOperation+secondNum+"=";}
+    let result=operate(currentOperation,firstNum,secondNum)
+    display1.textContent=result
+    numButtons.forEach(number=>number.removeEventListener("click",makeSecondVar));
+    numButtons.forEach(number=>number.removeEventListener("click", fillFirstDisplayFirsTime))
+}
+let equals = document.querySelector(".equals");
+equals.addEventListener("click",evaluate)
+
+const clear=document.querySelector(".clear");
+clear.addEventListener("click",()=>{location.reload()})
+
+function backspace(){
+    if(display1.textContent==firstNum){
+        let len=firstNum.length
+        firstNum=firstNum.slice(0,len-1);
+        display1.textContent=firstNum;
+    }
+    if(display1.textContent==secondNum){
+        let len=secondNum.length
+        secondNum=secondNum.slice(0,len-1);
+        display1.textContent=secondNum;
+    }
+}
+const del=document.querySelector(".delete");
+del.addEventListener("click",backspace)
+
 function add(x,y){
     return Number(x)+Number(y)
 }
@@ -31,78 +107,3 @@ function operate(operator,x,y){
     }
     return res.toFixed(8).replace(/\.?0+$/, "")
 }
-
-let firstNum=""
-function fillFirstDisplayFirsTime (){
-    firstNum+=this.value;
-    display1.textContent=firstNum;
-    if (this.value==="."){
-        decimal.removeEventListener("click",fillFirstDisplayFirsTime)}
-}
-const decimal=document.querySelector(".decimal")
-const display1=document.querySelector(".display1")
-
-const numButtons=document.querySelectorAll(".num");
-numButtons.forEach(number=>number.addEventListener("click", fillFirstDisplayFirsTime))
-
-let operatorVar="";
-let currentNum="";
-let secondNum="";
-let currentOperation="";
-function addEventListenerOperator(){
-    operatorVar=this.value;
-    if (operatorVar===currentOperation||currentOperation==="") {   
-        currentNum=operate(operatorVar,firstNum,secondNum);
-        display2.textContent=currentNum+operatorVar;}
-    else{
-        currentNum=operate(currentOperation,firstNum,secondNum);
-        display2.textContent=currentNum+operatorVar;
-    }
-    display1.textContent="_";
-    firstNum=currentNum;
-    secondNum="";
-    currentOperation=operatorVar
-    numButtons.forEach(number=>number.removeEventListener("click", fillFirstDisplayFirsTime))
-    numButtons.forEach(number=>number.addEventListener("click",makeSecondVar))
-}
-function makeSecondVar(){
-    secondNum+=this.value;
-    display1.textContent=secondNum;
-    if (this.value==="."){
-        decimal.removeEventListener("click",makeSecondVar)}
-}
-const display2=document.querySelector(".display2");
-const operatorButtons=document.querySelectorAll(".operator");
-operatorButtons.forEach(operator=>operator.addEventListener("click",addEventListenerOperator));
-
-let equals = document.querySelector(".equals");
-equals.addEventListener("click",()=>{
-    if(secondNum===""){
-        display2.textContent=firstNum+"=";
-    }
-    else{display2.textContent=firstNum+currentOperation+secondNum+"=";}
-    let result=operate(currentOperation,firstNum,secondNum)
-    display1.textContent=result
-    numButtons.forEach(number=>number.removeEventListener("click",makeSecondVar));
-    numButtons.forEach(number=>number.removeEventListener("click", fillFirstDisplayFirsTime))
-})
-
-const clear=document.querySelector(".clear");
-clear.addEventListener("click",()=>{location.reload()})
-
-const del=document.querySelector(".delete");
-del.addEventListener("click",()=>{
-    if(display1.textContent==firstNum){
-        let len=firstNum.length
-        firstNum=firstNum.slice(0,len-1);
-        display1.textContent=firstNum;
-    }
-    if(display1.textContent==secondNum){
-        let len=secondNum.length
-        secondNum=secondNum.slice(0,len-1);
-        display1.textContent=secondNum;
-    }
-})
-
-
-
